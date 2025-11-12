b806be4a14c5c73f5980c020385c8a23dd73629e
maurya050 <mauryashivam050@gmail.com>
2025-11-12 16:15:38 +0530

    Home page updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,23 @@ const Home = () => {
       <h1 className='text-center'>The Hub for every Tech <br/> Event that you can't miss.</h1>
       <p className='text-center mt-5'>Hackathons, Conferences, Meetups, and More, All in One Place.</p>
       <ExploreBtn />
+
+      <div className='mt-20 space-y-7'>
+
+        <h3>Featured Events</h3>
+
+        <ul className='events'>
+          {[1,2,3,4].map((event) => (
+            <li key={event} className="event-card">
+              <h4>Event Title {event}</h4>
+              <p>Date: 2024-07-0{event} | Location: City {event}</p>
+              <p className='mt-3'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque nisl eros, pulvinar facilisis justo mollis, auctor consequat urna.</p>
+              <a href="#" className='text-sm mt-5 inline-block underline'>Learn More</a>
+            </li>
+          ))}
+
+        </ul>
+      </div>
     </section>
   )
 }
--- src/components/ExploreBtn.tsx
+++ src/components/ExploreBtn.tsx
@@ -3,9 +3,9 @@ import Image from "next/image";
 const ExploreBtn = () => {
   return (
     <button type="button" id="explore-btn" className="mt-7 mx-auto" onClick={() => console.log("button")}>
-        <a href="#events" className="px-6 py-3 bg-blue-600 text-white rounded-md hover:bg-blue-700 transition">
+        <a href="#events"  >
             Explore Events
-            <Image src="/icons/arrow-down.svg" alt="Arrow Down" width={16} height={16}  />
+            <Image src="/icons/arrow-down.svg" alt="Arrow Down" width={24} height={24}  />
         </a>    
     </button>
   )
